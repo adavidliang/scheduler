040b6ef636fc452dda78998763ffb5bad0aee3d4
adavidliang <adavidliang@gmail.com>
2023-07-21 19:16:09 -0400

    import all the query form react-testing/library, and using getAllByTestId to write testing case

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import { waitForElement, render, fireEvent } from "@testing-library/react";
+import { waitForElement, render, fireEvent, getByText, prettyDOM, getAllByTestId } from "@testing-library/react";
 import Application from 'components/Application';
 
-describe("application",( ) => {
+describe("application", () => {
 
   it("defaults to Monday and changes the schedule when a new day is selected", async () => {
     const { getByText } = render(<Application />);
-    
+
 
     await waitForElement(() => getByText("Monday"))
-      fireEvent.click(getByText("Tuesday"))
-      expect(getByText("Leopold Silvers")).toBeInTheDocument()
-    
+    fireEvent.click(getByText("Tuesday"))
+    expect(getByText("Leopold Silvers")).toBeInTheDocument()
+
 
   });
 
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+
+    const { container } = render(<Application />);
+    
+    await waitForElement(() => getByText(container, "Archie Cohen")
+    )
+    console.log(prettyDOM(container));
+    const appointments = getAllByTestId(container, "appointment") 
+    console.log(prettyDOM(appointments));
+    const appointment = getAllByTestId(container, "appointment")[0];
+    console.log(prettyDOM(appointment))
+  })
+
 
 })
+
+
